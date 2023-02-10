4bd5f662190dc1e46c104d4ff5e4ba888dde5922
王重阳 <122137545+wxhn98@users.noreply.github.com>
2023-02-10 19:20:21 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include<stdio.h>
-#define width 30
-int starbar();
-int main(void)
-
+int imin(int,int);
+int main()
 {
-    starbar();
-    printf("            ");
-    printf("hhhh\n");
-    starbar();
+    int evi11;
+    int evi12;
+
+    printf("Enter a pair of integer (q to quit):\n");
+    while (scanf("%d%d",&evi11,&evi12)==2)
+    {
+     printf("The lesser of %d and %d is %d.\n",
+            evi11,evi12,imin(evi11,evi12));
+    }
+    printf("Bye.\n");
     return 0;
 }
-
-int starbar()
+int imin(int n, int m)
 {
-    int count;
-    for(count=1;count<=width;count++)
-        putchar('*');
-    putchar('\n');
-    return 0;
-}
+    int min;
 
+    if(n<m)
+        min=n;
+    else
+        min=m;
+
+    return min;
+}
