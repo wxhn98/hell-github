54129f2cf983ae521ce506956f02f4cf94ce7cf6
王重阳 <122137545+wxhn98@users.noreply.github.com>
2023-02-13 15:32:22 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,11 @@
 #include<stdio.h>
-int imin(int,int);
 int main()
 {
-    int evi11;
-    int evi12;
-
-    printf("Enter a pair of integer (q to quit):\n");
-    while (scanf("%d%d",&evi11,&evi12)==2)
-    {
-     printf("The lesser of %d and %d is %d.\n",
-            evi11,evi12,imin(evi11,evi12));
-    }
-    printf("Bye.\n");
+    int ten=10;
+    int two=2;
+    printf("Doing is right:");
+    printf("%d minus %d is %d \n",ten,2,ten-two);
+    printf("Doing it wrong:");
+    printf("%d minus %d is %d\n",ten,2,ten+two);
     return 0;
-}
-int imin(int n, int m)
-{
-    int min;
-
-    if(n<m)
-        min=n;
-    else
-        min=m;
-
-    return min;
 }
